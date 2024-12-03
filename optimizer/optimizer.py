a24e67b24cccffea90dfa80b07fc16f15efa9434
Vrajesh Vaghasiya <vrajesh.vaghasiya@elixirtechne.com>
2024-12-03 11:03:34 +0530

    Added create task function.

--- optimizer/optimizer.py
+++ optimizer/optimizer.py
@@ -19,21 +19,50 @@ class Optimizer:
             setattr(cls, self, instance)
         return getattr(cls, self)
 
-    def __init__(self, api_domain, api_schema):
+    def __init__(self, api_domain, api_schema, access_key):
         super().__init__()
         self.API_DOMAIN = api_domain
         self.API_SCHEME = api_schema
+        self.access_key = access_key
         self._session = RetrySession(self)
 
-    def get_task(self, task_id):
+    def get_task(self, task_id: str) -> any:
         url = urlunparse((self.API_SCHEME, self.API_DOMAIN, f"{PATHS['TASK_PATH']}/{task_id}", '', '', ''))
-        params = {}
-        resp = self._session.get(url, params={}, verify=HTTPS_CERTIFICATE_LOCATION)
+        headers = {'access-key': self.access_key}
+        resp = self._session.get(url, headers=headers, verify=HTTPS_CERTIFICATE_LOCATION)
         self._handle_api_resp(method='GET', url=url, resp=resp)
         if not resp.json():
             logger.error(f"Failed to call the API.")
             raise OptimizerAPIException("API call failed.")
         return resp.json()
+
+    def create_task(self, num_of_tasks: int, model: str, temprature: int, max_token: int, input_token: int) -> dict:
+        assert isinstance(num_of_tasks, int)
+        assert isinstance(model, str)
+        assert isinstance(temprature, int)
+        assert isinstance(max_token, int)
+        assert isinstance(input_token, int)
+
+        payload = {
+            "model": model,
+            "temperature": temprature,
+            "max_token": max_token,
+            "input_token": input_token
+        }
+        url = urlunparse((self.API_SCHEME, self.API_DOMAIN, f"{PATHS['TASK_PATH']}", '', '', ''))
+        responses = {}
+        for i in range(num_of_tasks):
+            payload['message_text'] = {"user": f"test question {i}"}
+            headers = {"access-key": self.access_key}
+            resp = self._session.post(json=payload, url=url, headers=headers)
+            self._handle_api_resp(method='GET', url=url, resp=resp)
+            if not resp.json():
+                logger.error(f"Failed to call the API.")
+                raise OptimizerAPIException("API call failed.")
+
+            responses[f'response_task_{i}'] = resp.json()
+
+        return responses
 
     def _handle_api_resp(self, method, url, resp):
         if not resp.ok:
